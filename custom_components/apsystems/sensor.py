6a956f03fb199bc99fc812bda31fc03b56f00c24
François Onimus <francois.onimus@gmail.com>
2025-01-06 21:05:33 +0100

    fix: issues

--- custom_components/apsystems/sensor.py
+++ custom_components/apsystems/sensor.py
@@ -319,8 +319,10 @@ class APsystemsFetcher:
             else:
                 self.cache = result_data.json()
 
-            _LOGGER.debug("lifetime data: " + str(result_data_lifetime.json().lifetime))
-            self.cache.lifetime = result_data_lifetime.json().lifetime
+            _LOGGER.debug("lifetime data: " + str(result_data_lifetime.json()))
+            responseDataJson = result_data_lifetime.json()
+            _LOGGER.debug(responseDataJson)
+            self.cache.lifetime = responseDataJson['lifetime']
 
             _LOGGER.debug(self.cache)
 
